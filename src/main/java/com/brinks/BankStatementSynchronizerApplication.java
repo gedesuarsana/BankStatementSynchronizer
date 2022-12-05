646dddca1f5e9f851bb4c171cafcf78d8ba12ba8
gedesuarsana <gedesuarsana11415@yahoo.com>
2022-12-05 14:06:32 +0700

    adding log

--- src/main/java/com/brinks/BankStatementSynchronizerApplication.java
+++ src/main/java/com/brinks/BankStatementSynchronizerApplication.java
@@ -2,12 +2,13 @@ package com.brinks;
 
 import com.brinks.models.InvoiceStatus;
 import com.brinks.repository.InvoiceStatusRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class BankStatementSynchronizerApplication implements CommandLineRunner {
@@ -18,12 +19,15 @@ public class BankStatementSynchronizerApplication implements CommandLineRunner {
     @Autowired
     InvoiceStatusRepository invoiceStatusRepository;
 
+    Logger logger = LoggerFactory.getLogger(BankStatementSynchronizerApplication.class);
+
     public static void main(String args[]) {
         SpringApplication.run(BankStatementSynchronizerApplication.class, args);
     }
 
     public void run(String... args) throws Exception {
-        System.out.println("test-->"+name);
+
+        logger.info("Program Starting!!!");
 
         InvoiceStatus invoiceStatus = new InvoiceStatus();
         invoiceStatus.setInvoice("invoice1");
